38b4ab6c5bb4f91edc8a52ea65434253477a97fb
Mehnazar <161054226+Mehnazar@users.noreply.github.com>
2024-11-10 01:16:21 +0500

    added header and pages

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import Header from "@/components/header/header";
 export default function CV() {
   return (
+    
     <div className="bg-gray-900 text-white p-8 md:p-16">
+      <Header/>
       <div className="max-w-4xl mx-auto bg-gray-800 rounded-lg shadow-lg p-8">
         {/* Profile Section */}
         <div className="flex justify-between items-center mb-8">
@@ -28,70 +31,7 @@ export default function CV() {
           Welcome to my portfolio! I am an accomplished Electronics Engineer with a Master's degree from Mehran University of Engineering & Technology, Jamshoro, and a Full Stack Developer with expertise in modern web and app development. With over a decade of experience in engineering and a passion for technology, I blend analytical problem-solving skills with advanced coding abilities to deliver innovative solutions.
           </p>
         </div>
-        {/* About Me Description */}
-        <div className="mb-8">
-          <h2 className="text-teal-400 text-2xl font-semibold">About me:</h2>
-          <p className="mt-2 text-sm leading-relaxed text-justify">
-          I completed my bachelor's in Electronics Engineering in 2007 and earned my master's degree in 2017 with a strong academic record (CGPA 3.57). My engineering background has equipped me with a deep understanding of electronic systems, circuit design, and complex problem-solving.
-
-          </p>
-        </div>
-
-        {/* Education Section */}
         
-        <div className="mb-8">
-          <h2 className="text-teal-400 text-2xl font-semibold">Education:</h2>
-          <div className="mt-4 space-y-4">
-            <div>
-              <h3 className="font-semibold">MEHRAN UET, JAMSHORO</h3>
-              <p className="text-sm">Master Degree Graduate (2017)</p>
-              <p className="text-xs">
-                Masters in Electronics System Engineering with CGPA:3.57 
-              </p>
-            </div>
-            <div>
-              <h3 className="font-semibold">MEHRAN UET, JAMSHORO</h3>
-              <p className="text-sm">Bachelor Degree Graduate (2007)</p>
-              <p className="text-xs">
-                B.E, Electonics Engineering with 1st Div 
-              </p>
-            </div>
-            <div>
-              <h3 className="font-semibold">GIAIC</h3>
-              <p className="text-sm">GIAIC  (2023)</p>
-              <p className="text-xs">
-               Currently studying in Governor Sindh Initiative for IT  
-              </p>
-            </div>
-            <div>
-              <h3 className=" text-teal-400 text-2xl font-semibold mt-4 p-2">Other Certifications:</h3>
-              <li> Wireless Local Loop</li>
-              <li> Auto CAD</li>
-              <li> Primavera Project Planner</li>
-               </div>
-          </div>
-        </div>
-
-        {/* Languages and Hobbies */}
-        <div className="grid grid-cols-2 gap-4 mb-8">
-          <div>
-            <h2 className="text-teal-400 text-2xl font-semibold">Languages:</h2>
-            <ul className="mt-2 space-y-1 text-sm">
-              <li>English</li>
-              <li>Urdu</li>
-              <li>Sindhi</li>
-            </ul>
-          </div>
-          <div>
-            <h2 className="text-teal-400 text-2xl font-semibold">Hobbies:</h2>
-            <ul className="mt-2 space-y-1 text-sm">
-              <li>Travel</li>
-              <li>Gaming</li>
-              <li>Gyming</li>
-              <li>Music</li>
-            </ul>
-          </div>
-        </div>
 
         {/* Skills Section */}
         <div className="mb-8">
@@ -153,13 +93,7 @@ export default function CV() {
           </div>
         </div>
 
-        {/* Contact Information */}
-        <div className="mt-8 border-t border-gray-700 pt-4">
-          <p className="text-xs">
-            Phone: +92-346-3863672 | Email: <a href="s.mehnazar@yahoo.com">s.mehnazar@yahoo.com</a> |<a href=" Github:github:https://github.com/Mehnazar">Github:github:https://github.com/Mehnazar</a> | Linkedin:<a href="linkedin:linkedin.com/in/mehnazar-umair-2972ba2ba">Mehnazar Umair</a>
-           | Address: Nazimabad, Khi,Sindh
-          </p>
-        </div>
+        
       </div>
     </div>
   );
